702c2df57229cb4cd566b2831f349cc96df4c29a
Beriholic <17323911768@163.com>
2023-08-03 18:31:27 +0800

    [update]添加空指针判断

--- util/video.go
+++ util/video.go
@@ -8,9 +8,13 @@ import (
 )
 
 func UpdateVideoInfo(userId int64, videos *[]*dao.Video) (*time.Time, error) {
-	videoSize := len(*videos)
-	if videos == nil || videoSize == 0 {
+	if videos == nil {
 		return nil, errors.New("[UpdateVideoInfo] video is nil")
+	}
+
+	videoSize := len(*videos)
+	if videoSize == 0 {
+		return nil, errors.New("[UpdateVideoInfo] video size is 0")
 	}
 
 	p := redis.NewProxyIndexMap()
