88e6e6681628c1da8e221abdca296afccfcf3a47
Beriholic <17323911768@163.com>
2023-08-19 22:18:36 +0800

    [add]添加判断条件

--- dao/userLoginDao.go
+++ dao/userLoginDao.go
@@ -14,6 +14,10 @@ type UserLogin struct {
 
 // 根据传入的用户名和密码查找数据
 func JudgeUserPassword(name, password string) (int64, error) {
+	if !IsExistUserLoginInfoByName(name) {
+		return 0, errors.New("该用户不存在")
+	}
+
 	userlogInfo := UserLogin{}
 	DB.Where("username=? and password=?", name, password).First(&userlogInfo)
 
