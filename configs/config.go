6113c9891acf7e717e28baffebe438853e34fd72
Beriholic <17323911768@163.com>
2023-08-24 10:22:34 +0800

    update

--- configs/config.go
+++ configs/config.go
@@ -18,11 +18,11 @@ const MAX_VIDEO_CNT = 30
 
 // mysql的连接信息
 const (
-	DB_USER   = "root"      // 数据库用户名
-	DB_PASSWD = "db22455"   //数据库密码
-	DB_URL    = "127.0.0.1" // 数据库地址
-	PORT      = "3306"      // 数据库端口
-	DB_NAME   = "tiktok"    //数据库名称
+	DB_USER   = "root"    // 数据库用户名
+	DB_PASSWD = "db22455" //数据库密码
+	DB_URL    = "mysql"   // 数据库地址
+	PORT      = "3306"    // 数据库端口
+	DB_NAME   = "tiktok"  //数据库名称
 )
 
 // GetDBInfo 返回mysql的连接信息
@@ -48,7 +48,7 @@ const VIDEO_COVER_SAVE_PATH = "static/assets/cover"
 const AVATAR_SAVE_PATH = "static/assets/avatar"
 
 const (
-	REDIS_URL  = "127.0.0.1"
+	REDIS_URL  = "redis"
 	REDIS_PORT = "6379"
 )
 
